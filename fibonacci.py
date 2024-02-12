70fc99f99bc9fd349ef7364977c783ba616449a4
medvedevmr <medvedevmr.work@gmail.com>
2024-02-12 21:44:26 +0100

    Recursive Functions

---
+++ fibonacci.py
@@ -0,0 +1,7 @@
+def fibonacci(position):
+    if position >= 3:
+        output = fibonacci(position - 1) + fibonacci(position - 2)
+    else:
+        output = 1
+        
+    return output
---
+++ recursive.py
@@ -0,0 +1,6 @@
+def count_to_num(num):
+    if num > 0:
+        count_to_num(num-1)
+        print(num)
+        
+count_to_num(10)
