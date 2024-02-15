7725fea74599e8859b0f4b4199d6722ab2a57b92
medvedevmr <medvedevmr.work@gmail.com>
2024-02-15 12:11:29 +0100

    Unary Operations Added

--- interpreter.py
+++ interpreter.py
@@ -39,18 +39,33 @@ class Interpreter:
             output = left / right
         
         return Integer(output) if left_type == right_type == "INT" else Float(output)
+    
+    def compute_unary(self,operator,operand):
+        operand_type = "VAR" if str(operand.type).startswith("VAR") else str(operand.type)
+        operand = getattr(self, f"read_{operand_type}")(operand.value)
         
+        if operator.value == '+':
+            return +operand
+        elif operand.value == '-':
+            return -operand
+            
     def interpret(self,tree=None):
         if tree is None:
             tree = self.tree
-        left_node = tree[0]
-        if isinstance(left_node,list):
-            left_node = self.interpret(left_node)
-        
-        right_node = tree[2]
-        if isinstance(right_node,list):
-            right_node = self.interpret(right_node)
-        
-        operator = tree[1]
-        
-        return self.compute_bin(left_node, operator, right_node)
+            
+        if isinstance(tree,list) and len(tree) == 2:
+            return self.compute_unary(tree[0],tree[1])
+        elif not isinstance(tree,list):
+            return tree
+        else:
+            left_node = tree[0]
+            if isinstance(left_node,list):
+                left_node = self.interpret(left_node)
+            
+            right_node = tree[2]
+            if isinstance(right_node,list):
+                right_node = self.interpret(right_node)
+            
+            operator = tree[1]
+            
+            return self.compute_bin(left_node, operator, right_node)
